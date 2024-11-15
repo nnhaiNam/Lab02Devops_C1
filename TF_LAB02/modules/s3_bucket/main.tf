1573b4adc459439a54bccd4efd724d6295655f53
Hai Nam <hainam000090@gmail.com>
2024-11-15 16:12:39 +0700

    test

--- TF_LAB02/modules/s3_bucket/main.tf
+++ TF_LAB02/modules/s3_bucket/main.tf
@@ -2,12 +2,17 @@ provider "aws" {
   region = "us-east-1" 
 }
 
-data "aws_s3_bucket" "existing_bucket" {
-  bucket = "bucket-hari-nem-nem"
+data "null_data_source" "existing_bucket_check" {
+  inputs = {
+    bucket_name = "bucket-hari-nem-nem"
+  }
 }
 
 resource "aws_s3_bucket" "s3" {
-  count  = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0 
+  #count  = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0 
+  #count  = length(try(data.aws_s3_bucket.existing_bucket.id, "")) == 0 ? 1 : 0
+  #count  = length(try(data.aws_s3_bucket.existing_bucket.id, "")) == 0 ? 1 : 0
+  count  = length(try(data.null_data_source.existing_bucket_check.outputs["bucket_name"], "")) == 0 ? 1 : 0
   bucket = "bucket-hari-nem-nem" 
 
   versioning {
