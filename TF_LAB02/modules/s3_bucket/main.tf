292a9ae55462d9048ea16337cf8d19840203a8af
Hai Nam <hainam000090@gmail.com>
2024-11-15 16:21:16 +0700

    test

--- TF_LAB02/modules/s3_bucket/main.tf
+++ TF_LAB02/modules/s3_bucket/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "s3" {
   #count  = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0 
   #count  = length(try(data.aws_s3_bucket.existing_bucket.id, "")) == 0 ? 1 : 0
   #count  = length(try(data.aws_s3_bucket.existing_bucket.id, "")) == 0 ? 1 : 0
-  count  = length(try(data.null_data_source.existing_bucket_check.outputs["bucket_name"], "")) == 0 ? 1 : 0
+  count  = length(try(data.null_data_source.existing_bucket_check.outputs["bucket_name"], "no")) == 2 ? 1 : 0
   bucket = "bucket-hari-nem-nem" 
 
   versioning {
