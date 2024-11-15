4cf03e97d3cb6419f3edc3400fe3899a437fb00f
Hai Nam <hainam000090@gmail.com>
2024-11-15 15:34:26 +0700

    fix

--- TF_LAB02/modules/s3_bucket/main.tf
+++ TF_LAB02/modules/s3_bucket/main.tf
@@ -2,12 +2,18 @@ provider "aws" {
   region = "us-east-1" 
 }
 
+data "aws_s3_bucket" "existing_bucket" {
+  bucket = "bucket-hari-nem-nem"
+}
+
 resource "aws_s3_bucket" "s3" {
+  count  = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0 
   bucket = "bucket-hari-nem-nem" 
 
   versioning {
     enabled = true
   }
+
   
   tags = {
     Name        = "Harinem_Bucket"
