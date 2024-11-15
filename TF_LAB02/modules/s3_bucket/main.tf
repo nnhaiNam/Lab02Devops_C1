a1b18b2f5abfc35a7b8f32c0264b0b788fc28c0a
Hai Nam <hainam000090@gmail.com>
2024-11-15 09:45:48 +0700

    skip test

--- TF_LAB02/modules/s3_bucket/main.tf
+++ TF_LAB02/modules/s3_bucket/main.tf
@@ -1,9 +1,14 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "bucketHarinemnem"
+resource "aws_s3_bucket" "s3" {
+  bucket = "bucketHarinemnem" 
 
+  versioning {
+    enabled = true
+  }
+  
   tags = {
     Name        = "Harinem_Bucket"
     Environment = "dev"
   }
+
 
 }
